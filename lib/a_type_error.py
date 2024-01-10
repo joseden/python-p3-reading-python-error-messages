7feb0618cbbbb64e62cdee55365413f0ee7371bd
Joseph <joseph.dena@student.moringaschool.com>
2024-01-10 09:22:33 +0300

    Complete Assignment

--- lib/a_type_error.py
+++ lib/a_type_error.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
 
-wrong_type = 'abc' + 123
+correct_type = 'abc' + '123'  # Concatenating two strings
+print(correct_type)
+
--- lib/an_assertion_error.py
+++ lib/an_assertion_error.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python3
 
-assert(1 == 2)
+# Using assert to check if 1 is equal to 1 (which is True)
+assert(1 == 1)
+# No AssertionError is raised, and the program continues
+print("Assertion passed!")
+
